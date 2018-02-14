848e06354471f5e708fcab4ba62d6026da6547b7
Kasper Hettinga <kasper.hettinga@gmail.com>
2018-02-14 10:28:29 +0100

    Rework towards simple Mackay bootanimation
    Change-Id: I62bf42d99b98bcabc7f0b215bb9f6e3b0a484e5a

--- config/common.mk
+++ config/common.mk
@@ -1,5 +1,33 @@
 PRODUCT_BRAND ?= LineageOS
 
+ifneq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
+# determine the smaller dimension
+TARGET_BOOTANIMATION_SIZE := $(shell \
+  if [ "$(TARGET_SCREEN_WIDTH)" -lt "$(TARGET_SCREEN_HEIGHT)" ]; then \
+    echo $(TARGET_SCREEN_WIDTH); \
+  else \
+    echo $(TARGET_SCREEN_HEIGHT); \
+  fi )
+
+# get a sorted list of the sizes
+bootanimation_sizes := $(subst .zip,,$(shell ls -1 vendor/lineage/prebuilt/common/bootanimation | sort -rn))
+
+# find the appropriate size and set
+define check_and_set_bootanimation
+$(eval TARGET_BOOTANIMATION_NAME := $(shell \
+  if [ -z "$(TARGET_BOOTANIMATION_NAME)" ]; then \
+    if [ "$(1)" -le "$(TARGET_BOOTANIMATION_SIZE)" ]; then \
+      echo $(1); \
+      exit 0; \
+    fi;
+  fi;
+  echo $(TARGET_BOOTANIMATION_NAME); ))
+endef
+$(foreach size,$(bootanimation_sizes), $(call check_and_set_bootanimation,$(size)))
+PRODUCT_BOOTANIMATION := vendor/cm/prebuilt/common/bootanimation/$(TARGET_BOOTANIMATION_NAME).zip
+endif
+
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
@@ -102,10 +130,6 @@ endif
 ifeq ($(WITH_TWRP),true)
 include vendor/lineage/config/twrp.mk
 endif
-
-# Bootanimation
-PRODUCT_PACKAGES += \
-    bootanimation.zip
 
 # Required Lineage packages
 PRODUCT_PACKAGES += \
--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -8,4 +8,10 @@ PRODUCT_PACKAGES += \
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
 
+
+ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
+    PRODUCT_COPY_FILES += \
+        vendor/cm/prebuilt/common/bootanimation/480.zip:system/media/bootanimation.zip
+endif
+
 $(call inherit-product, vendor/lineage/config/telephony.mk)
